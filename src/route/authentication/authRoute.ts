7708717b2be717b533ae711fc0bd4e3b4c601637
emils <emilsbernhards2001@gmail.com>
2021-03-31 20:43:57 +0200

    Removed log

--- src/route/authentication/authRoute.ts
+++ src/route/authentication/authRoute.ts
@@ -5,7 +5,7 @@ var router = express.Router()
 router.post("/login", (req, res) => {
     // Read username and password from request body
     const { username, password } = req.body;
-    console.log(username, password)
+    
     // Filter user from the users array by username and password
     let user;
     if (username === process.env.DEV_USERNAME && password === process.env.DEV_PASSWORD) {

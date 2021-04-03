3320894676190e08a681495047d8331cf8202e6b
emils <emilsbernhards2001@gmail.com>
2021-04-03 12:51:59 +0200

    CD

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -3,6 +3,7 @@ var express = require("express")
 var router = express.Router()
 const crypto = require('crypto')
 
+
 router.post("/", (req, res, next) => {
     const modifiedFiles = req.body.head_commit.modified
 

65af05e66b60c73268bebf5ae4d623a9404f5fc1
emils <emilsbernhards2001@gmail.com>
2021-04-03 14:44:55 +0200

    CD

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -4,6 +4,7 @@ const { exec } = require('child_process');
 
 var router = express.Router()
 
+
 router.post("/", (req, res, next) => {
    
     // const modifiedFiles = req.body.head_commit.modified

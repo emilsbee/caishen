7d60d082ba3d1b3bd6d0baec30bb934c8d773ab6
emils <emilsbernhards2001@gmail.com>
2021-04-03 14:43:52 +0200

    CD

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -4,15 +4,18 @@ const { exec } = require('child_process');
 
 var router = express.Router()
 
-
 router.post("/", (req, res, next) => {
    
     // const modifiedFiles = req.body.head_commit.modified
-    exec("git reset --hard")
-    exec("git clean -df")
-    exec("git pull -f")
-    exec("yarn run start")
+    exec("git reset --hard", execCallback)
+    exec("git clean -df", execCallback)
+    exec("git pull -f", execCallback)
+    exec("yarn run start2", execCallback)
     res.status(200).send()
 })
 
+function execCallback(err, stdout, stderr) {
+    if (stdout) console.log(stdout)
+    if (stderr) console.error(stderr)
+}
 export default router

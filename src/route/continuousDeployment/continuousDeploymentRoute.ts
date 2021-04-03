7d4339662b95605e1cf8107ca808e9edbba37202
emils <emilsbernhards2001@gmail.com>
2021-04-03 12:42:16 +0200

    CD

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -4,7 +4,10 @@ var router = express.Router()
 
 
 router.post("/", (req, res, next) => {
-
+    const modifiedFiles = req.body.head_commit.modified
+    const headers = req.headers
+    console.log("Modified files: ", modifiedFiles)
+    console.log("Headers: ", headers)
     res.status(200).send()
 })
 

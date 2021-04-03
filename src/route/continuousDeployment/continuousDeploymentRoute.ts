5d8e52c66ef1b84f5df2b2dc22d2f7e12e87bcf2
emils <emilsbernhards2001@gmail.com>
2021-04-03 14:26:19 +0200

    CD

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -1,33 +1,19 @@
 // External imports
 var express = require("express")
-const crypto = require("crypto")
-const bodyParser = require("body-parser")
+const { exec } = require('child_process');
+
 var router = express.Router()
 
-router.use(bodyParser.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf
-    }
-}))
 
 
 router.post("/", (req, res, next) => {
    
     // const modifiedFiles = req.body.head_commit.modified
-    const sigHeaderName = 'X-Hub-Signature-256'
-    const sigHashAlg = 'sha256'
-
-    const reqSign = req.get(sigHeaderName)
-
-
-    const hmac = crypto.createHmac(sigHashAlg, req.rawBody)
-    const digested = hmac.digest("hex")
-    
-    console.log(digested)
-    console.log(reqSign)
+    exec("git reset --hard")
+    exec("git clean -df")
+    exec("git pull -f")
+    exec("yarn run start")
     res.status(200).send()
-    
-
 })
 
 export default router

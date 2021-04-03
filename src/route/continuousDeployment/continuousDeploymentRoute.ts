330d6e1ee7c9aa456d0d426eba8bef638cca612e
emils <emilsbernhards2001@gmail.com>
2021-04-03 12:05:27 +0200

    Github webhook route

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -1,13 +1,21 @@
 // External imports
 var express = require("express")
 var router = express.Router()
+const { Webhooks, createNodeMiddleware } = require("@octokit/webhooks");
 
-
-// Internal imports
-
+const webhooks = new Webhooks({
+  secret: process.env.GITHUB_WEBHOOK_SECRET,
+});
 
 router.post("/", (req, res, next) => {
-    console.log(req)
+    let signature = webhooks.sign(req.body);
+    let verified = webhooks.verify(req.body, signature);
+
+    if (verified) {
+        console.log("Valid!")
+    } else {
+        console.log("Invalid")
+    }
 })
 
 export default router

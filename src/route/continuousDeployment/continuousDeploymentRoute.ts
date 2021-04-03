8de0b649c8a437466777f600dfd678b9fd9d0730
emils <emilsbernhards2001@gmail.com>
2021-04-03 14:03:30 +0200

    CD

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -1,29 +1,26 @@
 // External imports
 var express = require("express")
+const crypto = require("crypto")
+
 var router = express.Router()
-const crypto = require('crypto');
 
 router.post("/", (req, res, next) => {
-    const modifiedFiles = req.body.head_commit.modified
-    
+   
+    // const modifiedFiles = req.body.head_commit.modified
     const sigHeaderName = 'X-Hub-Signature-256'
     const sigHashAlg = 'sha256'
 
-    const bufBody = Buffer.from(req.body)
+    const reqSign = req.get(sigHeaderName)
 
-    const sig = Buffer.from(req.get(sigHeaderName), 'utf8')
-    const hmac = crypto.createHmac(sigHashAlg, process.env.GITHUB_WEBHOOK_SECRET)
-    const digest = Buffer.from(sigHashAlg + '=' + hmac.update(bufBody.toString()).digest('hex'), 'utf8')
+    const strObj = JSON.stringify(req.body)
+
+    const hmac = crypto.createHmac(sigHashAlg, strObj)
+    const digested = hmac.digest("hex")
     
+    console.log(digested)
+    console.log(reqSign)
+    res.status(200).send()
     
-    if(sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
-        console.log("Invalid")
-        next({code: 400, message:`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`})
-    } else {
-        console.log("Valid")
-        res.status(200).send()
-    }
-    console.log("After")
 
 })
 

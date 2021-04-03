ecc3b9ec45f036b5129c7217b324b5fb591c1243
emils <emilsbernhards2001@gmail.com>
2021-04-03 12:27:43 +0200

    CD test

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -1,22 +1,12 @@
 // External imports
 var express = require("express")
 var router = express.Router()
-const { Webhooks, createNodeMiddleware } = require("@octokit/webhooks");
 
 
-const webhooks = new Webhooks({
-  secret: process.env.GITHUB_WEBHOOK_SECRET,
-});
 
 router.post("/", (req, res, next) => {
-    let signature = webhooks.sign(req.body);
-    let verified = webhooks.verify(req.body, signature);
 
-    if (verified) {
-        console.log("Valid!")
-    } else {
-        console.log("Invalid")
-    }
+    res.status(200).send()
 })
 
 export default router

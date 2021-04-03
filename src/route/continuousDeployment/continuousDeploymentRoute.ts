754218fc38926ed0f337d91069b2925ecaa00e70
emils <emilsbernhards2001@gmail.com>
2021-04-03 12:51:26 +0200

    CD

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -1,13 +1,25 @@
 // External imports
 var express = require("express")
 var router = express.Router()
+const crypto = require('crypto')
 
 router.post("/", (req, res, next) => {
     const modifiedFiles = req.body.head_commit.modified
-    const headers = req.headers
-    console.log("Modified files: ", modifiedFiles)
-    console.log("Headers: ", headers)
-    res.status(200).send()
+
+    const sigHeaderName = 'X-Hub-Signature-256'
+    const sigHashAlg = 'sha256'
+
+    const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
+    const hmac = crypto.createHmac(sigHashAlg, process.env.GITHUB_WEBHOOK_SECRET)
+    const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8')
+    
+    if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+        next({code: 400, message:`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`})
+    } else {
+        console.log("Valid")
+        res.status(200).send()
+    }
+
 })
 
 export default router

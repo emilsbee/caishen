b09df088ecd0d143467a01d0de9ab8b7048025e4
emils <emilsbernhards2001@gmail.com>
2021-04-03 13:20:29 +0200

    CD

--- src/route/continuousDeployment/continuousDeploymentRoute.ts
+++ src/route/continuousDeployment/continuousDeploymentRoute.ts
@@ -9,17 +9,17 @@ router.post("/", (req, res, next) => {
     const sigHeaderName = 'X-Hub-Signature-256'
     const sigHashAlg = 'sha256'
 
-    // const signature = req.get(sigHeaderName)
+    const signature = req.get(sigHeaderName)
     const hmac = crypto.createHmac(sigHashAlg, process.env.GITHUB_WEBHOOK_SECRET)
-    const digest = sigHashAlg + "=" + hmac.update(req.body).digest("hex")
+    const digest = sigHashAlg + "=" + hmac.update(JSON.stringify(req.body)).digest("hex")
 
-    // if(signature.length !== digest.length || !crypto.timingSafeEqual(digest, signature)) {
-    //     console.log("Invalid")
-    //     next({code: 400, message:`Request body digest (${digest}) did not match ${sigHeaderName} (${signature})`})
-    // } else {
-    //     console.log("Valid")
-    // }
-    res.status(200).send()
+    if(signature.length !== digest.length || !crypto.timingSafeEqual(digest, signature)) {
+        console.log("Invalid")
+        next({code: 400, message:`Request body digest (${digest}) did not match ${sigHeaderName} (${signature})`})
+    } else {
+        console.log("Valid")
+        res.status(200).send()
+    }
 
 
 })
